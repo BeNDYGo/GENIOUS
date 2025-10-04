c22cc5183a150e0d1053fe095f271dc46856fabf
BeNDYGo <palexascha@gmail.com>
2025-10-04 20:34:02 +0300

    visual changes

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const SampleSearch = 'https://genius.com/search?q=';
 const SampleButtonHTML = `
 <button id="ButtonSearchGenius" 
-        style="background: none; border: none; color:rgb(206, 206, 206); font-size: 18px; font-weight: 600; 
+        style="background: none; border: none; color:rgb(153, 153, 153); font-size: 18px; font-weight: 600; 
                padding: 8px 12px; cursor: pointer; transition: color 0.3s ease, transform 0.2s ease; 
                position: relative; z-index: 1000;">
     G
